8abefb01060beba051d86a4b1766a0c903e6726a
Aymeric Augustin <aymeric.augustin@m4x.org>
2016-09-03 19:10:49 +0200

    Update setup.py according to PyPA guidelines.

--- setup.py
+++ setup.py
@@ -1,49 +1,44 @@
-import os
+from __future__ import unicode_literals
+
+import codecs
+import os.path
 
 import setuptools
 
-# Avoid polluting the .tar.gz with ._* files under Mac OS X
-os.putenv('COPYFILE_DISABLE', 'true')
+root_dir = os.path.abspath(os.path.dirname(__file__))
 
-# Prevent distutils from complaining that a standard file wasn't found
-README = os.path.join(os.path.dirname(__file__), 'README')
-if not os.path.exists(README):
-    os.symlink(README + '.rst', README)
+description = ("REplicated STOrage for Django, file backends "
+               "that mirror media files to several servers over HTTP")
 
-description = ('REplicated STOrage for Django, file backends '
-               'that mirror media files to several servers over HTTP')
-
-with open(README) as f:
-    long_description = '\n\n'.join(f.read().split('\n\n')[2:8])
+with codecs.open(os.path.join(root_dir, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='django-resto',
     version='1.3',
-    author='Aymeric Augustin',
-    author_email='aymeric.augustin@m4x.org',
-    url='https://github.com/aaugustin/django-resto',
     description=description,
     long_description=long_description,
-    download_url='http://pypi.python.org/pypi/django-resto',
+    url='https://github.com/aaugustin/django-resto',
+    author='Aymeric Augustin',
+    author_email='aymeric.augustin@m4x.org',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
     packages=[
         'django_resto',
     ],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-    ],
-    platforms='all',
-    license='BSD'
 )
